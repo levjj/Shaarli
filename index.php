ee67e7b3f2ff879388529e1115efecb4dd2d06e4
Christopher Schuster <cs@livoris.de>
2015-03-27 23:48:55 -0700

    use protocol-relative url

--- index.php
+++ index.php
@@ -437,9 +437,9 @@ if (isset($_POST['login']))
 // You can append $_SERVER['SCRIPT_NAME'] to get the current script URL.
 function serverUrl()
 {
-    $https = (!empty($_SERVER['HTTPS']) && (strtolower($_SERVER['HTTPS'])=='on')) || $_SERVER["SERVER_PORT"]=='443'; // HTTPS detection.
+    // $https = (!empty($_SERVER['HTTPS']) && (strtolower($_SERVER['HTTPS'])=='on')) || $_SERVER["SERVER_PORT"]=='443'; // HTTPS detection.
     $serverport = ($_SERVER["SERVER_PORT"]=='80' || ($https && $_SERVER["SERVER_PORT"]=='443') ? '' : ':'.$_SERVER["SERVER_PORT"]);
-    return 'http'.($https?'s':'').'://'.$_SERVER["SERVER_NAME"].$serverport;
+    return '//'.$_SERVER["SERVER_NAME"].$serverport;
 }
 
 // Returns the absolute URL of current script, without the query.
